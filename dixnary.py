e2dceeeabcafd1880176847e0920a40c5e3c7bbd
Akash <akashpatelp83@gmail.com>
2022-07-05 16:59:16 +0530

    Create Dixnary.py

---
+++ dixnary.py
@@ -0,0 +1,46 @@
+import requests
+
+
+word = input("\t\tWELCOME !!\nType the word >>> ")
+w = word.lower()
+api_url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{w}"
+
+response = requests.get(api_url)
+response.encoding = 'utf-8'
+content = response.json()
+
+try:
+    repo = content[0]['meanings']
+    print(f"\nDefinitions for {word} ---->>>>\n")
+    if len(repo) > 0:
+
+        for r in range(len(repo)):
+
+            part_of_speech = repo[r]['partOfSpeech']
+            word_meanings = repo[r]['definitions']
+            print(f"PART OF SPEECH : {part_of_speech}\n")
+
+            for wm in word_meanings:
+                try:
+                    definition = wm['definition']
+                    example = wm['example']
+                    synonyms = wm['synonyms']
+                    antonyms = wm['antonyms']
+
+                    print(f"MEANING : {definition}")
+                    print(f"EXAMPLE : {example}\n")
+
+                    if len(synonyms) > 0:
+                        print(f"SYNONYMS : {synonyms}\n")
+
+                    if len(antonyms) > 0:
+                        print(f"ANTONYMS : {antonyms}\n")
+
+                except KeyError:
+                    pass
+
+    else:
+        print("!! Please check the spellings.")
+
+except KeyError:
+    print("Check spellings")
